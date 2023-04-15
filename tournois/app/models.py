f5d4480fe8112abfb01c2b893a1ae0866d891f14
Manon Delucenay <manon.delucenay@student.isae-supaero.fr>
2023-04-15 19:47:08 +0200

    Modèles

--- tournois/app/models.py
+++ tournois/app/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
-# Create your models here.
+# Create your models here. 
+class Match(models.Model):
+    time = models.DateTimeField()
+    team1 = models.ForeignKey(Team, on_delete=models.CASCADE)
+    team2 = models.ForeignKey(Team, on_delete=models.CASCADE)
+    score1 = models.IntegerField()
+    score2 = models.IntegerField()
+    poule = models.PositiveIntegerField()
+
+class Players(models.Model):
+    name = models.CharField(max_length=150)
+    
+class Team(models.Model):
+    name = models.CharField(max_length=100)
+    trainer = models.CharField(max_length=200)
+    teammates = models.ManyToManyField(Players)
+    
+class Poule(models.Model):
+    nb = models.IntegerField()
+    tournament = models.CharField(max_length=100)
+    teams = models.ManyToManyField(Team)
+
+class Tournament(models.Model):
+    name = models.CharField(max_length=200)
+    teams = models.ManyToManyField(Team)
+    nb_poules = models.IntegerField()
+    team_per_poules = models.IntegerField()
+    place = models.CharField(max_length=100, blank=True)
+    start_date = models.DateField(blank=True)
+    end_date = models.DateField(blank=True)
