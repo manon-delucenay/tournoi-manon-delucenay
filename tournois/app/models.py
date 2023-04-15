c14e04e03f1d4513ea32b5b5976a2fc6c771ac6f
Manon Delucenay <manon.delucenay@student.isae-supaero.fr>
2023-04-15 23:11:20 +0200

    Migrations

--- tournois/app/models.py
+++ tournois/app/models.py
@@ -1,14 +1,6 @@
 from django.db import models
 
 # Create your models here. 
-class Match(models.Model):
-    time = models.DateTimeField()
-    team1 = models.ForeignKey(Team, on_delete=models.CASCADE)
-    team2 = models.ForeignKey(Team, on_delete=models.CASCADE)
-    score1 = models.IntegerField()
-    score2 = models.IntegerField()
-    poule = models.PositiveIntegerField()
-
 class Players(models.Model):
     name = models.CharField(max_length=150)
     
@@ -17,16 +9,23 @@ class Team(models.Model):
     trainer = models.CharField(max_length=200)
     teammates = models.ManyToManyField(Players)
     
-class Poule(models.Model):
+class Pool(models.Model):
     nb = models.IntegerField()
     tournament = models.CharField(max_length=100)
     teams = models.ManyToManyField(Team)
+    
+class Match(models.Model):
+    time = models.DateTimeField()
+    teams = models.ManyToManyField(Team)
+    score1 = models.IntegerField()
+    score2 = models.IntegerField()
+    pool = models.PositiveIntegerField()
 
 class Tournament(models.Model):
     name = models.CharField(max_length=200)
     teams = models.ManyToManyField(Team)
-    nb_poules = models.IntegerField()
-    team_per_poules = models.IntegerField()
+    nb_pool = models.IntegerField()
+    team_per_pools = models.IntegerField()
     place = models.CharField(max_length=100, blank=True)
     start_date = models.DateField(blank=True)
     end_date = models.DateField(blank=True)
